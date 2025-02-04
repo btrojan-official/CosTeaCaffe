ee6565f21c642aafd9e1103d6a8c5f588a542851
btrojan-official <bartosztrojanofficial@gmail.com>
2025-02-04 12:18:05 +0100

    initail commit 2

--- app/src/main/java/com/example/coteacaffe/MainActivity.kt
+++ app/src/main/java/com/example/coteacaffe/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.coteacaffe
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var listCat: ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        listCat = findViewById(R.id.listCat)
+
+        listCat.setOnItemClickListener{ _, _, position, _ ->
+            when(position){
+                1 -> startActivity(Intent(this, DrinkCategoryActivity::class.java))
+            }
         }
     }
 }
